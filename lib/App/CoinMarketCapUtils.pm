d029e2b044acc08917633e6b0683d37495308ef3
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2018-01-07 08:30:08 +0700

    Initial commit

---
+++ lib/App/CoinMarketCapUtils.pm
@@ -0,0 +1,18 @@
+package App::CoinMarketCapUtils;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: CLI utilities related to https://coinmarketcap.com website
+
+=head1 DESCRIPTION
+
+This distribution contains the following CLI utilities:
+
+# INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+=cut
